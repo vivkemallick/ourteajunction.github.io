e6f57fb53486c8978e8de9304567e6eced1ca15a
vivkemallick <vivekmallick09@gmail.com>
2021-06-24 23:01:55 +0530

    our tea junction website

---
+++ application/views/home.php
@@ -0,0 +1,314 @@
+
+<header id="header">
+  <div class="intro">
+    <div class="overlay">
+      <div class="container">
+        <div class="row">
+          <div class="intro-text">
+            <h1> Our Tea Junction</h1>
+            <p>Reservations: (+91) 8017-054-117</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<!-- Features Section -->
+<div id="features" class="text-center">
+  <div class="container">
+    <div class="section-title">
+      <h2>Our Specials</h2>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-4">
+        <div class="features-item">
+          <h3>Tandoor Tea</h3>
+          <img src="<?php echo base_url();?>assets/img/specials/120595056_2707613359504180_3143188916845960416_n.jpg" class="img-responsive" alt="">
+          <p>Hot Tandoor Tea With Chicken Pakoda Combo.</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4">
+        <div class="features-item">
+          <h3>Kesar Pesta Tea</h3>
+          <img src="<?php echo base_url();?>assets/img/specials/IMG-20210618-WA0020.jpg" class="img-responsive" alt="">
+          <p>With Magic Of Pista, You just can't control your Hunger.</p>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4">
+        <div class="features-item">
+          <h3>Chicken Gravy Burger</h3>
+          <img src="<?php echo base_url();?>assets/img/specials/burger.jpg" class="img-responsive" alt="">
+          <p>Jumbo Chicken Gravy Burger With A Cup of Hot Chocolate Tea.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- About Section -->
+<div id="about">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-md-6 about-img"> </div>
+      <div class="col-xs-12 col-md-3 col-md-offset-1">
+        <div class="about-text">
+          <div class="section-title">
+            <h2>Our Story</h2>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Restaurant Menu Section -->
+<div id="restaurant-menu">
+  <div class="container">
+    <div class="section-title text-center">
+      <h2>Menu</h2>
+    </div>
+    <div class="row">
+      <!-- <div class="col-xs-12 col-sm-6">
+        <div class="menu-section">
+          <h2 class="menu-section-title">Breakfast & Starters</h2>
+          <div class="menu-item">
+            <div class="menu-item-name">Tortellini Skewers</div>
+            <div class="menu-item-price"> $9 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Fried Ravioli</div>
+            <div class="menu-item-price"> $7 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Wild Mushroom Arancini</div>
+            <div class="menu-item-price"> $9 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Mozzarella Sticks</div>
+            <div class="menu-item-price"> $10 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+        </div>
+      </div> -->
+      <div class="col-xs-12 col-sm-12">
+        <div class="menu-section">
+
+          <h2 class="menu-section-title">Our Main Course</h2>
+          <div class="menu-item">
+            <div class="menu-item-name">Tandoor Tea</div>
+            <div class="menu-item-price">From Rs. 15 / 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Kesar Pesta Tea</div>
+            <div class="menu-item-price">From Rs. 15 / 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Hot Chocolate Tea</div>
+            <div class="menu-item-price">From Rs. 15 / 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Special Malai Tea</div>
+            <div class="menu-item-price">From Rs. 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Tandoor Kesar Pesta Tea</div>
+            <div class="menu-item-price">From Rs. 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Masala Tea</div>
+            <div class="menu-item-price">From Rs. 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Tandoor Masala Tea</div>
+            <div class="menu-item-price">From Rs. 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Tandoor Kesar Pesta Tea</div>
+            <div class="menu-item-price">From Rs. 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Green Tea</div>
+            <div class="menu-item-price">From Rs. 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Lemon Tea</div>
+            <div class="menu-item-price">From Rs. 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Black Tea</div>
+            <div class="menu-item-price">From Rs. 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Special Tea</div>
+            <div class="menu-item-price">From Rs. 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Special Tea</div>
+            <div class="menu-item-price">From Rs. 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-12">
+        <div class="menu-section">
+          <h2 class="menu-section-title">Starter</h2>
+          <div class="menu-item">
+            <div class="menu-item-name">Chicken Pakoda</div>
+            <div class="menu-item-price">From Rs. 30 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Chicken Gravy Burger</div>
+            <div class="menu-item-price">Rs. 60 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Special Tea & Chicken Pakoda Combo</div>
+            <div class="menu-item-price"> Rs. 99 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam.. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Tandoor Tea With Chicken Gravy Burger Combo </div>
+            <div class="menu-item-price"> Rs. 99 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+        </div>
+      </div>
+     <!--  <div class="col-xs-12 col-sm-6">
+        <div class="menu-section">
+          <h2 class="menu-section-title">Desserts</h2>
+          <div class="menu-item">
+            <div class="menu-item-name">Chocolate Mud Cake</div>
+            <div class="menu-item-price"> $11 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Bourbon-Pecan Tart</div>
+            <div class="menu-item-price"> $14 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Texas Sheet Cake</div>
+            <div class="menu-item-price"> $15 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+          <div class="menu-item">
+            <div class="menu-item-name">Vanilla Cheesecake</div>
+            <div class="menu-item-price"> $18 </div>
+            <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+          </div>
+        </div>
+      </div> -->
+    </div>
+  </div>
+</div>
+<!-- Gallery Section -->
+<div id="gallery">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-6 col-md-3">
+        <div class="gallery-item"> <img src="<?php echo base_url();?>assets/img/gallery/IMG-20210618-WA0026.jpg" class="img-responsive" alt=""></div>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <div class="gallery-item"> <img src="<?php echo base_url();?>assets/img/gallery/IMG-20210618-WA0021.jpg" class="img-responsive" alt=""></div>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <div class="gallery-item"> <img src="<?php echo base_url();?>assets/img/gallery/120603367_2707613442837505_6848455256721553345_n.jpg" class="img-responsive" alt=""></div>
+      </div>
+      <div class="col-xs-6 col-md-3">
+        <div class="gallery-item"> <img src="<?php echo base_url();?>assets/img/gallery/120612179_2707613479504168_8194486097925683566_n.jpg" class="img-responsive" alt=""></div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Team Section -->
+<div id="team">
+  <div class="container">
+    <div id="row">
+      <div class="col-md-6">
+        <div class="col-md-10 col-md-offset-1">
+          <div class="section-title">
+            <h2>Meet Our Chef</h2>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare.</p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="team-img"><img src="<?php echo base_url();?>assets/img/gallery/120556554_2707613392837510_765933665979236448_n.jpg" alt="..."></div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Contact Section -->
+<div id="contact" class="text-center">
+  <div class="container text-center">
+    <div class="col-md-4">
+      <h3>Reservations</h3>
+      <div class="contact-item">
+        <p>Please call</p>
+        <p>(+91) 8017-054-117</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3>Address</h3>
+      <div class="contact-item">
+        <p>Chinsurah, West Bengal 712103</p>
+        <p> 398, Protappur</p>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <h3>Opening Hours</h3>
+      <div class="contact-item">
+        <p>Tue-Sun: 04:30 PM - 09:30 PM</p>
+        <p>COVID-19: 05:00 PM - 08:00 PM</p>
+      </div>
+    </div>
+  </div>
+  <!-- <div class="container">
+    <div class="section-title text-center">
+      <h3>Send us a message</h3>
+    </div>
+    <div class="col-md-8 col-md-offset-2">
+      <form name="sentMessage" id="contactForm" novalidate>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" id="name" class="form-control" placeholder="Name" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="email" id="email" class="form-control" placeholder="Email" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <textarea name="message" id="message" class="form-control" rows="4" placeholder="Message" required></textarea>
+          <p class="help-block text-danger"></p>
+        </div>
+        <div id="success"></div>
+        <button type="submit" class="btn btn-custom btn-lg">Send Message</button>
+      </form>
+    </div>
+  </div> -->
+</div>
---
+++ application/views/layout/footer.php
@@ -0,0 +1,24 @@
+<div id="footer">
+  <div class="container text-center">
+    <div class="col-md-6">
+      <p>&copy; 2021 OUR pvt ltd. All rights reserved. Design by <a href="http://www.templatewire.com" rel="nofollow">Vivek Mallick</a></p>
+    </div>
+    <div class="col-md-6">
+      <div class="social">
+        <ul>
+          <!-- <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-youtube"></i></a></li> -->
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.js"></script> 
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/contact_me.js"></script> 
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/main.js"></script>
+</body>
+</html>
